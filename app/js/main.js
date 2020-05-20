324c3e33ab934f72d8910bfa86045a03462b86d8
Your Namemauler2007 <mauler2007@gmail.com>
2020-05-20 13:40:28 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 $(function(){
 
-    
-    var mixer = mixitup('.products__inner-box');
-    
 
+   
+
+
+    var mixer = mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 4,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      });
 
 
 });
